298cfe1fef87690e8a124ba42585f4769eb4522e
will-17173 <huangzhaojian@gmail.com>
2017-03-14 13:31:03 +0800

    'init'

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ browserSync({
         baseDir: "src",
         index: "index.html"
     },
+    proxy: 'http://onfocus.win',
     port:10001,
     files: ["src/*.html", "src/css/*.css", "src/app.dist.js"]
 });
