4cf635b9e9d8fc9fa2c09619b1021c1ab0e4a6c7
will-17173 <huangzhaojian@gmail.com>
2017-03-14 15:12:32 +0800

    'init'

--- server.js
+++ server.js
@@ -1,10 +1,5 @@
-var browserSync = require('browser-sync');
+var express = require('express');
+var app = express();
 
-browserSync({
-    server: {
-        baseDir: "src",
-        index: "index.html"
-    },
-    port: 10001,
-    files: ["src/*.html", "src/css/*.css", "src/app.dist.js"]
-});
+app.use(express.static('src'));
+app.listen(3000);
