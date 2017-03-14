049c4fe2891050181386335641118b433886d585
will-17173 <huangzhaojian@gmail.com>
2017-03-14 13:08:22 +0800

    change server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var browserSync = require('browser-sync');
 
 browserSync({
-server: {
-  baseDir: "src",
-  index: "index.html"
-},
-  files: ["src/*.html", "src/css/*.css", "src/app.dist.js"]
+    server: {
+        baseDir: "src",
+        index: "index.html"
+    },
+    files: ["src/*.html", "src/css/*.css", "src/app.dist.js"]
 });
