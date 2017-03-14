31905984a908fe1dcadb66a0a1fbbcfa82f2f6e0
will-17173 <huangzhaojian@gmail.com>
2017-03-14 12:42:15 +0800

    'init'

---
+++ server.js
@@ -0,0 +1,9 @@
+var browserSync = require('browser-sync');
+
+browserSync({
+server: {
+  baseDir: "src",
+  index: "index.html"
+},
+  files: ["src/*.html", "src/css/*.css", "src/app.dist.js"]
+});
