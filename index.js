68abaaffe7f8ba4d355e15f9c4e34aa939e402de
Will.H <huangzhaojian@gmail.com>
2018-12-06 17:48:17 +0800

    init

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+var fs = require('fs');
+var path = __dirname + '../../services/photo';
+
+fs.readdir(path,function(err,files){
+  //获取目录所有图片，并生成文件
+  // console.log(files);
+  var msg = JSON.stringify(files);
+  console.log(msg)
+  // fs.writeFile("img/图片名字.json", msg, function(){
+  //   console.log("添加日志成功");
+  // });
+})
+
 app.use('/public', express.static(__dirname + '../../services/photo'))
 
 app.get('/', (req, res) => {
