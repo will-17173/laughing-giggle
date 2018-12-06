ebc467c051ae58cb37938397089b9e03c3ec9b2d
Will.H <huangzhaojian@gmail.com>
2018-12-06 19:34:14 +0800

    init

---
+++ db.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+const path = require('path')
+
+const dirs = [
+  '../../services/photo/JianGuo',
+  '../../services/photo/PRO7'
+]
+
+const names = [
+  'JianGuo',
+  'PRO7'
+]
+
+function makeJSON(dir, i){
+  const files = fs.readdirSync(dir);
+  var fileList = []
+  files.forEach((file, j) => {
+    if(!fs.lstatSync(file).isDirectory() && path.extname(file).toLowerCase() === 'jpg'){
+      fileList.push(path.basename(file))
+    }
+  })
+  fs.writeFileSync(`./db/${names[i]}.json`, JSON.stringify(fileList))
+}
+
+dirs.forEach((dir, i) => {
+  makeJSON(dir, i)
+})
