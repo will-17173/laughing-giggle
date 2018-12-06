d04757cb00abdd8f2c41eb3c1eb260d05b587560
Will.H <huangzhaojian@gmail.com>
2018-12-06 17:00:30 +0800

    init

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static(__dirname + '../../services/photo'))
+app.use('/public', express.static(__dirname + '../../services/photo'))
 
 app.get('/', (req, res) => {
   res.send('hello world')
