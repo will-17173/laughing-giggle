de579d1912a9233cb46ec4053d0f578ca2d9b75a
Will.H <huangzhaojian@gmail.com>
2018-12-06 19:54:22 +0800

    init

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cors = require('cors');
+app.use(cors());
 app.use(express.static('../../services/photo'))
+app.get('*', function(){});
 app.get('/', (req, res) => {
   res.send('hello world')
 })
+
 app.listen(port)
