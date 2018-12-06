e119f2deb0b7f3c70fb319c0b68516159d3b72ca
Will.H <huangzhaojian@gmail.com>
2018-12-06 19:16:48 +0800

    init

--- index.js
+++ index.js
@@ -1,48 +1,6 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const fs = require('fs')
-const path = require('path')
-const image = require("imageinfo");
-// const photoDir = path.join(__dirname, '../../services/photo/JianGuo');
-const photoDir = '../../services/photo/JianGuo/'
-
-function readFileList (path, filesList) {
-  var files = fs.readdirSync(path)
-  files.forEach(function (itm, index) {
-    var stat = fs.statSync(path + itm)
-    if (stat.isDirectory()) {
-      // 递归读取文件
-      readFileList(path + itm + '/', filesList)
-    } else {
-      var obj = {} // 定义一个对象存放文件的路径和名字
-      obj.path = path // 路径
-      obj.filename = itm // 名字
-      filesList.push(obj)
-    }
-  })
-}
-var getFiles = {
-  // 获取文件夹下的所有文件
-  getFileList: function (path) {
-    var filesList = []
-    readFileList(path, filesList)
-    return filesList
-  },
-  // 获取文件夹下的所有图片
-  getImageFiles: function (path) {
-    var imageList = []
-
-    this.getFileList(path).forEach(item => {
-      var ms = image(fs.readFileSync(item.path + item.filename))
-
-      ms.mimeType && imageList.push(item.filename)
-    })
-    return imageList
-  }
-}
-
-console.log(getFiles.getImageFiles(photoDir))
 
 app.use(express.static('../../services/photo'))
 
