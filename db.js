3ba6b748fe5fce2c796e436cc0f2f276830c1252
Will.H <huangzhaojian@gmail.com>
2018-12-06 19:35:12 +0800

    init

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const path = require('path')
 
 const dirs = [
-  '../../services/photo/JianGuo',
-  '../../services/photo/PRO7'
+  '../../services/photo/JianGuo/',
+  '../../services/photo/PRO7/'
 ]
 
 const names = [
